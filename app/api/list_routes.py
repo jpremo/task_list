9c41f676524e78573f02ff404d24fda25255dd9d
Jacob <premojacob@gmail.com>
2021-03-25 12:21:53 -0400

    Setup basic list_create route with validations

--- app/api/list_routes.py
+++ app/api/list_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 from app.models import List, db
+from app.forms import ListCreateForm
 
 list_routes = Blueprint('lists', __name__)
 
@@ -12,3 +13,20 @@ def lists_get():
     lists = List.query.all()
     legible_lists = [l.to_dict() for l in lists]
     return {'lists': legible_lists}
+
+
+@list_routes.route('/', methods=['POST'])
+def list_create():
+    """
+    Creates a new list
+    """
+    form = ListCreateForm(csrf_enabled=False)
+    if form.validate_on_submit():
+        data = request.get_json(force=True)
+        newList = List(title=data['title'])
+        db.session.add(newList)
+        db.session.commit()
+        return newList.to_dict()
+
+    error_msgs = form.errors
+    return {'errors': error_msgs}
